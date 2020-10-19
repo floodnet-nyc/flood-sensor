1ccc533a0d4843ba8017c8c8abe5d028b64ee697
Praneeth Challagonda <58634765+Praneethsvch@users.noreply.github.com>
2020-10-19 14:33:03 -0400

    ttn credentials file

---
+++ src/ttncredentials.h
@@ -0,0 +1,15 @@
+#ifndef TTNCREDENTIALS_H
+#define TTNCREDENTIALS_H
+
+// Enter your keys here
+
+// This EUI must be in little-endian format For TTN issued EUIs the last bytes should be 0xD5, 0xB3, 0x70.
+#define TTN_APPEUI {  }
+
+// This should also be in little endian format
+#define TTN_DEVEUI  {  }
+
+// This key should be in big endian format
+#define TTN_APPKEY {  }
+
+#endif

ffe15eef972f717cf55f4c938469446d94391296
Praneeth <psc368@nyu.edu>
2021-08-10 11:56:21 -0400

    Update lorawan.cpp
    modified lorawan.cpp to match the v3-app-change branch

--- src/lorawan.cpp
+++ src/lorawan.cpp
@@ -2,6 +2,11 @@
 #include "Arduino.h"
 #include "sensorcfg.h"
 #include "maxbotix.h"
+#include <EEPROM.h>
+#include "sensorcfg.h"
+#ifdef __asr650x__
+#include "innerWdt.h"
+#endif
 
 /*
  set LoraWan_RGB to Active,the RGB active in loraWan
@@ -11,6 +16,24 @@
  RGB yellow means RxWindow2;
  RGB green means received done;
  */
+
+/* Watchdog timer */
+bool autoFeed = false;
+
+/* EEPROM params */
+// start reading from the first byte (address 0) of the EEPROM
+const int defaultAppAddress = 0; // 0 to 7
+const int selectionAddress = 8; // Single char (y/n) in ASCII
+const int newAppAddress = 12;    // 12 to 19
+
+byte value;
+int joinFailCounter = 0;          // counter for Join failure
+unsigned char cfg_packet[7];      // CFG Uplink Packet
+unsigned char lora_packet[5];     // Regular Uplink Packet
+bool UPDATE_CONFIG = true;        // Set to true at start and when there is a change in sensor cfg; used to send sensor cfg via uplink
+unsigned int ERROR_FLAGS;
+bool init_ = false;
+char useOrigApp = 'y';
 
 /* OTAA para*/
 uint8_t devEui[] = {  };
@@ -53,7 +76,7 @@ uint8_t appPort = 2;
  receive an acknowledgment. The MAC performs a datarate adaptation,
  according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
  to the following table:
- 
+
  Transmission nb | Data Rate
  ----------------|-----------
  1 (first)       | DR
@@ -64,7 +87,7 @@ uint8_t appPort = 2;
  6               | max(DR-2,0)
  7               | max(DR-3,0)
  8               | max(DR-3,0)
- 
+
  Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
  the datarate, in case the LoRaMAC layer did not receive an acknowledgment
  */
@@ -73,6 +96,39 @@ uint8_t confirmedNbTrials = 4;
 static TimerEvent_t sleep;
 static TimerEvent_t wakeUp;
 uint8_t lowpower = 1;
+
+void JoinFailCounter(void) {
+    joinFailCounter = joinFailCounter + 1;
+}
+
+bool JoinFailed(void) {
+    if (joinFailCounter == 5) {
+        joinFailCounter = 0;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void JoinFailureDebug(void) {
+    if(JoinFailed() && useOrigApp == 'n') {
+
+        // Use old AppEUI
+        useOrigApp = 'y';
+        EEPROM.write(selectionAddress, (byte)(useOrigApp-'0'));
+
+        // discard failed AppEUI
+        for (int i = 0; i < 8; ++i)
+        {
+            EEPROM.write(newAppAddress + i, 0);
+        }
+
+        // restart
+        innerWdtEnable(autoFeed);
+    } else {
+        Serial.println("Join falied. Check the default Keys loaded while flashing the MCU!");
+    }
+}
 
 void onSleep()
 {
@@ -93,8 +149,152 @@ void onWakeUp()
     //  TimerStart( &sleep );
 }
 
+void ModifyDutyCycle(void){
+    unsigned long dutycycle = 0;
+    for (int i = 1; i < 3; i++) {
+        dutycycle =  (mcpsIndication->Buffer[i]) | ( dutycycle << 8*i);
+    }
+    if (dutycycle!= 0) {
+        Serial.print("Current duty cycle is: ");
+        Serial.println(TX_INTERVAL);
+        // Changing Duty Cycle
+        TX_INTERVAL = dutycycle;
+        Serial.print("Updated dutycycle is: ");
+        Serial.println(TX_INTERVAL);
+    } else{
+        Serial.println("Dutycycle is the same.");
+    }
+}
+
+void ModifySensorMode(void){
+    unsigned int sensorMode_ = 0;
+    sensorMode_ =  (mcpsIndication->Buffer[3]) | ( sensorMode_ );
+    if (sensorMode_!= 0 && sensorMode<=3 ) {
+        Serial.print("Current sensorMode is: ");
+        Serial.println(sensorMode);
+        // Changing Sensor Mode
+        sensorMode = sensorMode_;
+        Serial.print("Updated sensorMode is: ");
+        Serial.println(sensorMode);
+    } else{
+        Serial.println("Sensor Mode is the same.");
+    }
+}
+
+void ModifySamplingRate(void){
+    unsigned int sampling_rate = 0;
+    sampling_rate =  (mcpsIndication->Buffer[4]) | ( sampling_rate );
+    sampling_rate =  (mcpsIndication->Buffer[5]) | ( sampling_rate << 8);
+    if (sampling_rate!= 0 ) {
+        Serial.print("Current sensor sampling rate is: ");
+        Serial.println(sensor_sampling_rate);
+        // Changing Sensor Mode
+        sensor_sampling_rate = sampling_rate;
+        Serial.print("Updated sensor sampling rate is: ");
+        Serial.println(sensor_sampling_rate);
+    } else{
+        Serial.println("Sensor sampling rate is the same.");
+    }
+}
+
+void ModifyNumberOfSamples(void){
+    unsigned int numb_readings = 0;
+    numb_readings =  (mcpsIndication->Buffer[6]) | ( numb_readings );
+    if (numb_readings!= 0 && numb_readings<20) {
+        Serial.print("Current number of readings per measurement: ");
+        Serial.println(sensor_numberOfReadings);
+        // Changing Sensor Mode
+        sensor_numberOfReadings = numb_readings;
+        Serial.print("Updated number of readings per measurement: ");
+        Serial.println(sensor_numberOfReadings);
+    } else{
+        Serial.println("Sensor number of readings per measurement is the same.");
+    }
+}
+
+void ModifySensorSettings(void){
+     switch(mcpsIndication->BufferSize) {
+       case 3:
+   ModifyDutyCycle();
+   ModifySensorMode();
+   break;
+case 4:
+   ModifyDutyCycle();
+   ModifySensorMode();
+   break;
+case 6:
+   ModifyDutyCycle();
+   ModifySensorMode();
+   ModifySamplingRate();
+   break;
+case 7:
+   ModifyDutyCycle();
+   ModifySensorMode();
+   ModifySamplingRate();
+   ModifyNumberOfSamples();
+   break;
+default:
+   Serial.println("Invalid Sensor settings received.");
+   break;
+}
+}
+
+void ModifyAppEUI(void){
+    for (int i = 0; i < 8; ++i)
+    {
+        EEPROM.write(newAppAddress + i, mcpsIndication->Buffer[i+1]); //big-endian
+    }
+    useOrigApp = 'n'; // Use new AppEUI on restart
+    EEPROM.write(selectionAddress, (byte)(useOrigApp - '0')); // Save the choice to Flash Memory
+    // restart
+    innerWdtEnable(autoFeed);
+}
+
+//downlink data handle function example
+void downLinkDataHandle(McpsIndication_t *mcpsIndication)
+{
+    Serial.printf("+REV DATA:%s,RXSIZE %d,PORT %d\r\n",mcpsIndication->RxSlot?"RXWIN2":"RXWIN1",mcpsIndication->BufferSize,mcpsIndication->Port);
+    Serial.print("+REV DATA:");
+    for(uint8_t i=0;i<mcpsIndication->BufferSize;i++)
+    {
+        Serial.printf("%02X",mcpsIndication->Buffer[i]);
+    }
+    Serial.println();
+    uint32_t color=mcpsIndication->Buffer[0]<<16|mcpsIndication->Buffer[1]<<8|mcpsIndication->Buffer[2];
+#if(LoraWan_RGB==1)
+  turnOnRGB(color,5000);
+  turnOffRGB();
+#endif
+  // process received downlink
+  /*
+     * Downlink Packet format:
+     *    |   oper     | Duty Cycle in seconds  | Sensor Mode | Sampling Rate  |  Number of readings per measurement  |
+     *    |  1 byte    |       2 bytes          |    1 byte   |    2 bytes     |        1 byte                        |
+     */
+    // set UPDATE_CONFIG to true
+    UPDATE_CONFIG = true;
+    switch(mcpsIndication->Buffer[0]) {
+        case 0x4D:
+            /* Change sensor Mode*/
+            ModifySensorSettings();
+            break;
+        case 0x41:
+            /* Change APPEUI*/
+            if (mcpsIndication->BufferSize == 9)
+            {
+                ModifyAppEUI();
+            } else {
+                Serial.println("Invalid AppKey size!");
+            }
+            break;
+        default:
+            Serial.println("Invalid downlink format!");
+            break;
+    }
+}
+
 uint16_t distance;
-uint16_t batlevel;
+uint16_t batLevel;
 
 /* Prepares the payload of the frame */
 static void prepareTxFrame( uint8_t port )
@@ -106,33 +306,141 @@ static void prepareTxFrame( uint8_t port )
      for example, if use REGION_CN470,
      the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
      */
-    // Maxbotix reading
-    distance = read_sensor_using_modes(sensorMode, sensor_sampling_rate, sensor_numberOfReadings);
-    Serial.print("Distance = ");
-    Serial.print(distance);
-    Serial.println(" mm");
-    // Battery
-    batlevel=getBatteryVoltage();
-    // Payload
-    appDataSize = 5;
-    appData[0] = 0x00;  // First byte is zero to match decoder on backend
-    appData[1] = lowByte(batlevel);
-    appData[2] = highByte(batlevel);
-    appData[3] = lowByte(distance);
-    appData[4] = highByte(distance);
+
+    byte lowbyte, highbyte, lowbat, highbat;
+    // Error
+    ERROR_FLAGS = pow(2,0)* SD_ERROR;
+
+    // Formatting payload
+    if (UPDATE_CONFIG == true) {
+        // Send Sensor Config via Uplink
+        /*
+         CFG update uplink Format:
+         | Error Flag  | Sensor Mode | Sensor Sampling Rate | Sensor Number of Readings |
+         |    255 (FF) |    1 byte   |      2 bytes         |        1 bytes            |
+         */
+        appDataSize = 7;
+        ERROR_FLAGS = 255;
+        appData[0] = (unsigned char)ERROR_FLAGS;
+        uint16_t TX_INTERVAL = appTxDutyCycle/1000;
+        byte lowduty = lowByte(TX_INTERVAL);
+        byte highduty = highByte(TX_INTERVAL);
+        appData[1] = (unsigned char)lowduty;
+        appData[2] = (unsigned char)highduty;
+        appData[3] = (unsigned char)sensorMode;
+        lowbyte = lowByte(sensor_sampling_rate);
+        highbyte = highByte(sensor_sampling_rate);
+        appData[4] = (unsigned char)lowbyte;
+        appData[5] = (unsigned char)highbyte;
+        appData[6] = (unsigned char)sensor_numberOfReadings;
+    } else {
+        // Regular Uplink contains: Sensor Error Flags followed by Battery and then Sensor Data
+
+        /* LoraWAN uplink packet format
+         | Error flags  | Battery Level | Ultrasonic reading  |
+         |   1 byte     |    2 bytes    |        2 bytes      |
+         |     Ultrasonic reading      |
+         |           2 bytes           |
+         |    high byte | low byte     |
+         |       Battery Level       |
+         |           2 bytes         |
+         |    high byte | low byte   |
+         |------------------------------------------------------------ Error Flags  ----------------------------------------------------------------|
+         |     bit 7                                                |  bit 6   |  bit 5  |  bit 4  |  bit 3  |  bit 2  |  bit 1  |      bit 0       |
+         |     Used only for CFG update (all other bits are high)   |          |         |         |         |         |         |   SD error flag  |
+         */
+
+        // Regular Uplink Packet size
+        appDataSize = 5;
+
+        // Maxbotix
+        distance = read_sensor_using_modes(sensorMode, sensor_sampling_rate, sensor_numberOfReadings);
+        Serial.print("Distance = ");
+        Serial.print(distance);
+        Serial.println(" mm");
+
+        // Battery
+        batLevel = getBatteryVoltage();
+
+        // Payload
+        lowbat = lowByte(batLevel);
+        highbat = highByte(batLevel);
+        appData[0] = (unsigned char)ERROR_FLAGS;
+        appData[1] = (unsigned char)lowbat; //we're unsigned
+        appData[2] = (unsigned char)highbat;
+        lowbyte = lowByte(distance);
+        highbyte = highByte(distance);
+        appData[3] = (unsigned char)lowbyte;
+        appData[4] = (unsigned char)highbyte;
+    }
     onSleep();
 }
 
+void InitStoreAPPEUI(void){
+    Serial.println("First time setup, Storing the original AppEUI...");
+    // set useOrigApp to 'y'
+    useOrigApp = 'y';
+    init_ = true;
+    int val = useOrigApp - '0';
+    EEPROM.write(selectionAddress, val);
+    // Save the default APPEUI in the Flash Memory
+    for (int i = 0; i < 8; ++i)
+    {
+        EEPROM.write(defaultAppAddress + i, appEui[i]); //big-endian
+    }
+    Serial.println("Default AppEUI saved in the Flash Memory.");
+}
+
+void LoadNewAppEUI(void){
+    bool foundNewAppEUI;
+    for (int i = 0; i < 8; ++i)
+    {
+        if((int)(EEPROM.read())!= 0){
+            // break out of the loop if atleast one non-zero byte is found
+            foundNewAppEUI = true;
+            break;
+        } else {
+            // all zeroes is default key
+            foundNewAppEUI = false;
+        }
+    }
+    if (foundNewAppEUI) {
+        // load new keys
+        Serial.println("New AppEUI found");
+        for (int i = 0; i < 8; ++i)
+        {
+        appEui[i] = (uint8_t)(EEPROM.read(newAppAddress + i)); //big-endian
+        }
+        Serial.println("New AppEUI loaded.");
+    } else {
+        Serial.println("Key is zeroes and not valid. Using the original AppEUI");
+        // key is zeroes and not valid
+        useOrigApp = 'y';
+        EEPROM.write(selectionAddress, (byte)(useOrigApp - '0');
+    }
+}
 
 void setup_lorawan(unsigned int packet_interval) {
+    Serial.println(F("Setting up LoraWAN..."));
+    // Set DutyCycle
     appTxDutyCycle = packet_interval*1000;
     boardInitMcu();
     pinMode(Vext, OUTPUT);
     digitalWrite(Vext, HIGH);
     Serial.begin(115200);
+    EEPROM.begin(512);
 #if (AT_SUPPORT)
     enableAt();
 #endif
+    useOrigApp = (int)(EEPROM.read(selectionAddress)) + '0';
+    if (useOrigApp == 'y' || useOrigApp == 'Y') {
+        // First time setup...
+        InitStoreAPPEUI();
+    }
+    if (useOrigApp == 'n' || useOrigApp == 'N') {
+        // Load new AppEUI
+        LoadNewAppEUI();
+    }
     deviceState = DEVICE_STATE_INIT;
     LoRaWAN.ifskipjoin();
     Serial.println("Setup done!");
@@ -140,7 +448,7 @@ void setup_lorawan(unsigned int packet_interval) {
     TimerInit( &wakeUp, onWakeUp );
     delay(500);
     onSleep();
-    
+
 }
 
 void lorawan_runloop_once(void)
@@ -163,6 +471,10 @@ void lorawan_runloop_once(void)
         case DEVICE_STATE_JOIN:
         {
             LoRaWAN.join();
+            JoinFailCounter();
+            if(JoinFailed()) {
+                JoinFailureDebug();
+            }
             break;
         }
         case DEVICE_STATE_SEND:

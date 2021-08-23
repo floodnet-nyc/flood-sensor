d21fe8a7ccf8839ddad648866d5418bf916e720c
Praneeth Challagonda <psc368@nyu.edu>
2021-08-23 12:52:21 -0400

    Update app-change.ino
    fixed EEPROM and MB bugs

--- examples/app-change/app-change.ino
+++ examples/app-change/app-change.ino
@@ -5,6 +5,8 @@
 #include "innerWdt.h"
 #endif
 
+
+#define triggerPin GPIO8
 /*
    set LoraWan_RGB to Active,the RGB active in loraWan
    RGB red means sending;
@@ -17,13 +19,13 @@
 // Enter your keys here
 
 // This EUI must be in big-endian format
-#define TTN_APPEUI {  }
+#define TTN_APPEUI { }
 
 // This should also be in big endian format
-#define TTN_DEVEUI  {  }
+#define TTN_DEVEUI { }
 
 // This key should be in big endian format
-#define TTN_APPKEY {  }
+#define TTN_APPKEY { }
 
 /* Watchdog timer */
 bool autoFeed = false;
@@ -61,7 +63,7 @@ LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
 DeviceClass_t  loraWanClass = LORAWAN_CLASS;
 
 /*the application data transmission duty cycle.  value in [ms].*/
-uint32_t appTxDutyCycle = 30000;
+uint32_t appTxDutyCycle = 15000;
 
 /*OTAA or ABP*/
 bool overTheAirActivation = LORAWAN_NETMODE;
@@ -99,6 +101,193 @@ uint8_t appPort = 2;
 */
 uint8_t confirmedNbTrials = 4;
 
+
+unsigned long maxTimeOut;
+
+// Maxbotix Ultrasonic variables
+uint16_t readings_arr[30] = {0};
+size_t n = *(&readings_arr + 1) - readings_arr;
+
+unsigned int sensorMode;
+unsigned int sensor_sampling_rate;
+unsigned int sensor_numberOfReadings;
+
+void printHex2(unsigned v) {
+  v &= 0xff;
+  if (v < 16)
+    Serial.print('0');
+  Serial.print(v, HEX);
+}
+
+void swap(uint16_t  *p, uint16_t  *q) {
+  int t;
+
+  t = *p;
+  *p = *q;
+  *q = t;
+}
+
+void sort(uint16_t a[], size_t n) {
+  int i, j, temp;
+
+  for (i = 0; i < n - 1; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      if (a[j] < a[j + 1])
+        swap(&a[j], &a[j + 1]);
+    }
+  }
+}
+
+uint16_t mean(uint16_t readings_arr[], size_t n, unsigned int sensor_numberOfReadings) {
+  uint16_t readings_sum = 0;
+  for (int i = 0; i < sensor_numberOfReadings; i++) {
+    readings_sum = readings_sum + readings_arr[i];
+  }
+  return readings_sum / sensor_numberOfReadings;
+}
+
+uint16_t median(uint16_t readings_arr[], size_t n, unsigned int sensor_numberOfReadings) {
+  if (sensor_numberOfReadings % 2 != 0) {
+    return readings_arr[(sensor_numberOfReadings + 1) / 2 - 1];
+  }
+  else {
+    return (readings_arr[sensor_numberOfReadings / 2 - 1] + readings_arr[sensor_numberOfReadings / 2]) / 2;
+  }
+}
+
+uint16_t mode(uint16_t readings_arr[], size_t n, unsigned int sensor_numberOfReadings) {
+  int counter = 1;
+  int max = 0;
+  uint16_t mode_ = readings_arr[0];
+  for (int i = 0; i < sensor_numberOfReadings - 1; i++) {
+    if ( readings_arr[i] == readings_arr[i + 1] ) {
+      counter++;
+      if ( counter > max ) {
+        max = counter;
+        mode_ = readings_arr[i];
+      }
+    } else
+      counter = 1;          // reset counter.
+  }
+  return mode_;
+}
+
+void setup_maxbotix(unsigned int mode = 2, unsigned int sampling_rate = 250 , unsigned int numberOfReadings = 7) {
+  Serial.println("Setting up Maxbotix .... ");
+  digitalWrite(Vext, HIGH);   //power line: now off
+  pinMode(triggerPin, OUTPUT);
+  digitalWrite(triggerPin, LOW);  //trigger line: now off
+  Serial1.begin(9600);
+  Serial.println("Sensor Settings:");
+  sensorMode = mode;
+  Serial.print("    Sensor mode: ");
+  Serial.println(sensorMode);
+  sensor_sampling_rate = sampling_rate;
+  Serial.print("    Sensor sampling rate: ");
+  Serial.println(sensor_sampling_rate);
+  sensor_numberOfReadings = numberOfReadings;
+  Serial.print("    Number of readings per measurement: ");
+  Serial.println(sensor_numberOfReadings);
+}
+
+uint16_t sensor_singleread(void) {
+  int distance = 0;
+  char serialbuffer[4];
+  int index = 0;
+  char rc;
+  Serial1.flush();
+  delay(150);
+  boolean newData = false;
+  while (newData == false) {
+    if (Serial1.available())
+    {
+      char rc = Serial1.read();
+      if (rc == 'R')
+      {
+        while (index < 3)
+        {
+          if (Serial1.available())
+          {
+            serialbuffer[index] = Serial1.read();
+            index++;
+          }
+        }
+      }
+      newData = true;
+    }
+  }
+  if (newData) {
+    distance = (serialbuffer[0] - '0') * 1000 + (serialbuffer[1] - '0') * 100 + (serialbuffer[2] - '0') * 10 + (serialbuffer[3] - '0');
+    Serial.print("distance: "); Serial.print(distance); Serial.println(" mm");
+  }
+  return distance;
+}
+
+
+uint16_t read_sensor_using_modes(unsigned int sensorMode, unsigned int sensor_sampling_rate = 20, unsigned int sensor_numberOfReadings = 5)
+{
+  uint16_t distance = 0;
+  Serial.println("Reading sensor using modes, entering measurements into an array...");
+  Serial.println("VEXT Low: Sensor Powered up");
+  digitalWrite(Vext, LOW);
+  digitalWrite(triggerPin, HIGH);
+  Serial.println("Trigger Pin High: Sensor reading...");
+  sensor_singleread();
+  delay(sensor_sampling_rate);
+  for (int i = 0; i < sensor_numberOfReadings; i++) {
+    readings_arr[i] = sensor_singleread();
+    delay(sensor_sampling_rate);
+  }
+  digitalWrite(triggerPin, LOW);
+  Serial.println("Trigger Pin Low: Sensor stopped.");
+  Serial.println("VEXT High: Sensor turned off");
+  digitalWrite(Vext, HIGH);
+
+  Serial.println("Printing the array...");
+  for (int i = 0; i < n; i++) {
+    Serial.print(readings_arr[i]); Serial.print(" ");
+  }
+  Serial.println("");
+  sort(readings_arr, n);
+  Serial.println("Printing the sorted array...");
+  for (int i = 0; i < n; i++) {
+    Serial.print(readings_arr[i]); Serial.print(" ");
+  }
+  Serial.println("");
+
+  switch (sensorMode) {
+    case 1:
+      // Mean
+      distance = mean(readings_arr, n, sensor_numberOfReadings);
+      break;
+    case 2:
+      // Median
+      distance = median(readings_arr, n, sensor_numberOfReadings);
+      break;
+    case 3:
+      // Mode
+      distance = mode(readings_arr, n, sensor_numberOfReadings);
+      break;
+    default:
+      // Single Pulse-In single reading
+      distance = sensor_singleread();
+      break;
+  }
+
+  Serial.println("Cleaning measurements array...");
+  for (int i = 0; i < n; i++) {
+    readings_arr[i] = 0;
+  }
+  Serial.println("");
+  Serial.println("Printing the cleaned array...");
+  for (int i = 0; i < n; i++) {
+    Serial.print(readings_arr[i]); Serial.print(" ");
+  }
+  Serial.println("");
+  return distance;
+
+}
+
 /* Prepares the payload of the frame */
 static void prepareTxFrame( uint8_t port )
 {
@@ -109,12 +298,42 @@ static void prepareTxFrame( uint8_t port )
     for example, if use REGION_CN470,
     the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
   */
-  appDataSize = 4;
-  appData[0] = 0x00;
-  appData[1] = 0x01;
-  appData[2] = 0x02;
-  appData[3] = 0x03;
+  // Regular Uplink Packet size
+  byte lowbyte, highbyte, lowbat, highbat;
+
+  // Regular Uplink Packet size
+  appDataSize = 5;
+
+  // Maxbotix
+  Serial.println("VEXT Low: Sensor Powered up");
+  digitalWrite(Vext, LOW);
+  digitalWrite(triggerPin, HIGH);
+  Serial.println("Trigger Pin High: Sensor reading...");
+  uint16_t  distance = read_sensor_using_modes(sensorMode, sensor_sampling_rate, sensor_numberOfReadings);
+  Serial.print("Distance = ");
+  Serial.print(distance);
+  Serial.println(" mm");
+  digitalWrite(triggerPin, LOW);
+  Serial.println("Trigger Pin Low: Sensor stopped.");
+  Serial.println("VEXT High: Sensor turned off");
+  digitalWrite(Vext, HIGH);
+
+  // Battery
+  uint16_t batLevel = getBatteryVoltage();
+
+  // Payload
+  lowbat = lowByte(batLevel);
+  highbat = highByte(batLevel);
+  appData[0] = (unsigned char)0;
+  appData[1] = (unsigned char)lowbat; //we're unsigned
+  appData[2] = (unsigned char)highbat;
+  lowbyte = lowByte(distance);
+  highbyte = highByte(distance);
+  appData[3] = (unsigned char)lowbyte;
+  appData[4] = (unsigned char)highbyte;
 }
+
+
 
 //downlink data handle function example
 void downLinkDataHandle(McpsIndication_t *mcpsIndication)
@@ -156,21 +375,6 @@ void downLinkDataHandle(McpsIndication_t *mcpsIndication)
         // Reset
         innerWdtEnable(false);
         delay(5000); //Wait until the MCU resets
-        //        // Load keys
-        //        EEPROM.begin(512);
-        //        LoadNewKeys();  // load keys
-        //        EEPROM.end();
-        //        Serial.println("Keys loaded...Start OTAA again...");
-        //        // take care of keys
-        //#if(LORAWAN_DEVEUI_AUTO)
-        //        LoRaWAN.generateDeveuiByChipID();
-        //#endif
-        //#if(AT_SUPPORT)
-        //        Serial.println("Getting device parameters...");
-        //        getDevParam();
-        //#endif
-        //        printDevParam();
-        //        deviceState = DEVICE_STATE_JOIN;
       } else {
         Serial.println("Invalid AppEUI and AppKey size!");
       }
@@ -310,6 +514,7 @@ void ifJoinedTTN(void) {
 void setup() {
   delay(3000);
   Serial.begin(115200);
+  setup_maxbotix(2, 250, 7);
   Serial.println(F("Checking keys..."));
   EEPROM.begin(512);
   // Check init_ for 'y' or 'n'

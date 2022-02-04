c417b19f2673323bb50395a704c92b174e13bfb9
Ric <rtoledocrow@gmail.com>
2022-02-04 11:02:46 -0500

    eeprom_clear note
    Added note to clear eeprom before uploading code

--- firmware/src/ttncredentials.h
+++ firmware/src/ttncredentials.h
@@ -3,6 +3,8 @@
 
 // Enter your keys here
 
+// NOTE: before uploading to the HTCC-AB02 run the Arduino: File>Examples> 'Examples for CubeCell-Board Plus (HTCC-AB02)' EEPROM > eeprom_clear
+
 // This EUI must be in little-endian format For TTN issued EUIs the last bytes should be 0xD5, 0xB3, 0x70.
 #define TTN_APPEUI {  }
 

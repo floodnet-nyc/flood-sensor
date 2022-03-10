cae1f3b738a08ccdb2f821b316072dfa37bfb746
Praneeth Challagonda <psc368@nyu.edu>
2022-03-10 15:43:52 -0500

    typo endianness
    all are big

--- firmware/src/ttncredentials.h
+++ firmware/src/ttncredentials.h
@@ -5,10 +5,10 @@
 
 // NOTE: before uploading to the HTCC-AB02 run the Arduino: File>Examples> 'Examples for CubeCell-Board Plus (HTCC-AB02)' EEPROM > eeprom_clear
 
-// This EUI must be in little-endian format For TTN issued EUIs the last bytes should be 0xD5, 0xB3, 0x70.
+// This EUI must be in big-endian format For TTN issued EUIs the last bytes should be 0xD5, 0xB3, 0x70.
 #define TTN_APPEUI {  }
 
-// This should also be in little endian format
+// This should also be in big endian format
 #define TTN_DEVEUI  {  }
 
 // This key should be in big endian format

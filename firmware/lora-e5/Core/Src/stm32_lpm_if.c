56e8b5a76901d6178c1522c909ef7e52b97fc70c
Praneeth <psc368@nyu.edu>
2022-09-09 19:16:19 -0400

    Low-power Stop2 mode

--- firmware/lora-e5/Core/Src/stm32_lpm_if.c
+++ firmware/lora-e5/Core/Src/stm32_lpm_if.c
@@ -2,6 +2,9 @@
 #include "platform.h"
 #include "stm32_lpm.h"
 #include "usart_if.h"
+#include "gpio.h"
+#include "sys_sensors.h"
+#include "usart.h"
 
 
 const struct UTIL_LPM_Driver_s UTIL_PowerDriver = {
@@ -16,12 +19,25 @@ void PWR_ExitOffMode(void) {}
 
 void PWR_EnterStopMode(void) {
   HAL_SuspendTick();
+  GPIO_InitTypeDef GPIO_InitStructure = {0};
+  GPIO_InitStructure.Pin = GPIO_PIN_All;
+  GPIO_InitStructure.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStructure.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
+  __HAL_RCC_GPIOA_CLK_DISABLE();
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
+  __HAL_RCC_GPIOB_CLK_DISABLE();
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
+  __HAL_RCC_GPIOC_CLK_DISABLE();
+  HAL_UART_MspDeInit(&hlpuart1);
   LL_PWR_ClearFlag_C1STOP_C1STB();
   HAL_PWREx_EnterSTOP2Mode(PWR_STOPENTRY_WFI);
 }
 
 void PWR_ExitStopMode(void) {
   HAL_ResumeTick();
+  MX_GPIO_Init();
+  HAL_UART_MspInit(&hlpuart1);
   vcom_Resume();
 }
 

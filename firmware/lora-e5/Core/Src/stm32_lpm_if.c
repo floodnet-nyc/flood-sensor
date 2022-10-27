ef0812f0dd63df98971bc53d8048f10d57c1062b
Praneeth <psc368@nyu.edu>
2022-10-27 18:26:59 -0400

    solar charger ic bug fix attempt

--- firmware/lora-e5/Core/Src/stm32_lpm_if.c
+++ firmware/lora-e5/Core/Src/stm32_lpm_if.c
@@ -1,16 +1,14 @@
 #include "stm32_lpm_if.h"
+#include "gpio.h"
 #include "platform.h"
 #include "stm32_lpm.h"
-#include "usart_if.h"
-#include "gpio.h"
 #include "sys_sensors.h"
 #include "usart.h"
-
+#include "usart_if.h"
 
 const struct UTIL_LPM_Driver_s UTIL_PowerDriver = {
-    PWR_EnterSleepMode, PWR_ExitSleepMode,
-    PWR_EnterStopMode,  PWR_ExitStopMode,
-    PWR_EnterOffMode,   PWR_ExitOffMode,
+    PWR_EnterSleepMode, PWR_ExitSleepMode, PWR_EnterStopMode,
+    PWR_ExitStopMode,   PWR_EnterOffMode,  PWR_ExitOffMode,
 };
 
 void PWR_EnterOffMode(void) {}
@@ -25,10 +23,18 @@ void PWR_EnterStopMode(void) {
   GPIO_InitStructure.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
   __HAL_RCC_GPIOA_CLK_DISABLE();
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
-  __HAL_RCC_GPIOB_CLK_DISABLE();
   HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
   __HAL_RCC_GPIOC_CLK_DISABLE();
+  GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_5 |
+                           GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 |
+                           GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 |
+                           GPIO_PIN_14 | GPIO_PIN_15;
+  GPIO_InitStructure.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStructure.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
+  HAL_GPIO_WritePin(BQ24210_PG_Port, BQ24210_PG_Pin, GPIO_PIN_RESET);
+  __HAL_RCC_GPIOB_CLK_DISABLE();
+
   HAL_UART_MspDeInit(&hlpuart1);
   LL_PWR_ClearFlag_C1STOP_C1STB();
   HAL_PWREx_EnterSTOP2Mode(PWR_STOPENTRY_WFI);

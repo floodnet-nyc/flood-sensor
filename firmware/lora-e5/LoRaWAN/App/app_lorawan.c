c4ebd734c64387edff540f29fd0070bc51936ee7
Praneeth <psc368@nyu.edu>
2023-02-22 15:28:08 -0500

    testing FatFs, turn off lorawan

--- firmware/lora-e5/LoRaWAN/App/app_lorawan.c
+++ firmware/lora-e5/LoRaWAN/App/app_lorawan.c
@@ -71,7 +71,7 @@ void MX_LoRaWAN_Init(void)
   /* USER CODE BEGIN MX_LoRaWAN_Init_2 */
 
   /* USER CODE END MX_LoRaWAN_Init_2 */
-  LoRaWAN_Init();
+//  LoRaWAN_Init();
   /* USER CODE BEGIN MX_LoRaWAN_Init_3 */
 
   /* USER CODE END MX_LoRaWAN_Init_3 */
